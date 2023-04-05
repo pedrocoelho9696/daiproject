c46a8a78a7f094a7419831624c33e0f7935e9cd2
pedrocoelho9696 <pedro.coelho9696@gmail.com>
2023-04-05 21:41:12 +0100

    uptade

---
+++ App.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import SigninScreen from './screens/SigninScreen';
+import { useState, useEffect } from 'react';
+import { auth } from 'firebase/auth';
+
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+        <Stack.Screen options={{ headerShown: false}} name="Signin" component={SigninScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import 'firebase/firestore';
+import { getFirestore, collection} from 'firebase/firestore';
+
+const firebaseConfig = {
+  // Your Firebase configuration object goes here
+  apiKey: "AIzaSyD2IjBIpJ5P42Wlj019I4MOOT7hL6BqVVM",
+  authDomain: "projetodai-73129.firebaseapp.com",
+  projectId: "projetodai-73129",
+  storageBucket: "projetodai-73129.appspot.com",
+  messagingSenderId: "567881372928",
+  appId: "1:567881372928:web:482112f08dc25814428fe5",
+  measurementId: "G-4B95SDGKKE"
+};
+
+const app = initializeApp(firebaseConfig, 'my-app');
+export const auth = getAuth(app);
+const firestore = getFirestore(app);
+export const db = getFirestore(app);
+
+export const usersRef = collection(db, 'users')
+
+
