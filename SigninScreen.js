bc07974734706fb9e957a82b09a269db15b144f7
pedrocoelho9696 <128489990+pedrocoelho9696@users.noreply.github.com>
2023-05-06 01:35:41 +0100

    Add files via upload

---
+++ SigninScreen.js
@@ -0,0 +1,175 @@
+import { useNavigation } from '@react-navigation/core'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import React, { useEffect, useState, useSelector } from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { auth, usersRef } from '../firebase'
+import {addDoc } from "firebase/firestore"
+
+
+const SigninScreen = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [role, setRole] = useState('')
+  const navigation = useNavigation()
+
+  const handleOptionPress = (option) => {
+    setRole(option);
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user =>{
+      if (user) {
+        navigation.replace("Home")
+      }
+    })
+    return unsubscribe
+  }, [])
+
+
+  const handleSignUp = async () => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      if (user) {
+        const doc = addDoc(usersRef, {
+          name,
+          password,
+          email,
+          role,
+          userId: user.uid,
+        });
+      } else {
+        throw new Error('User not found');
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      alert(error.message);
+    });
+  };
+
+  
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior= 'height'
+    >
+      <View style={styles.inputContainer}>
+      <TextInput
+          placeholder="Name"
+          value={name}
+          onChangeText={text => setName(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+        <Text style={styles.input}>Role </Text>
+      <TouchableOpacity 
+        onPress={() => handleOptionPress('Jogadora')}
+        style={{ 
+          backgroundColor: role === 'Jogadora' ? 'yellow' : '#ecf0f1',
+          padding: 10,
+          borderRadius: 5,
+          marginBottom: 5,
+          marginTop: 3
+        }}
+      >
+        <Text style={{ color: role === 'Jogadora' ? '#fff' : '#2c3e50' }}>Jogadora</Text>
+      </TouchableOpacity>
+      <TouchableOpacity 
+        onPress={() => handleOptionPress('Equipa Técnica')}
+        style={{ 
+          backgroundColor: role === 'Equipa Técnica' ? 'yellow' : '#ecf0f1',
+          padding: 10,
+          borderRadius: 5,
+        }}
+      >
+        <Text style={{ color: role === 'Equipa Técnica' ? '#fff' : '#2c3e50' }}>Equipa Técnica</Text>
+        
+      </TouchableOpacity>
+      
+
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Create account</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Login')}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Go back</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  )
+}
+
+export default SigninScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#a9a9a9'
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+    
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: 'yellow',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginBottom: 5
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+})
---
+++ TabScreen.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+const TabScreen = ({activeScreen, setActiveScreen}) => {
+  
+  return (
+    <View style={{backgroundColor: 'black',width: 200, height: 6000, borderTopWidth: 150, borderTopColor: 'grey', position: undefined}}>
+        
+    </View>
+  )
+};
+
+export default TabScreen;
